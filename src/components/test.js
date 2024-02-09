2e791a0e918148b48b519035734287287ff64ad0
lakshyeahh <verma.lakshya071@gmail.com>
2024-02-10 01:29:15 +0530

    Environment

---
+++ src/components/test.js
@@ -0,0 +1,14 @@
+import {React, Fragment} from 'react';
+
+
+const test = () => {
+    return (
+       <Fragment>
+        <h1>hello</h1>
+       </Fragment>
+    );
+};
+
+
+
+export default test;
